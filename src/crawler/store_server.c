8a363293cd328389d2b45d2ef58be6057aeef25d
Louca GUERMEUR <louca.guermeur@epita.fr>
2023-01-27 11:29:33 +0100

    save in repository folder in a safe way

--- src/crawler/store_server.c
+++ src/crawler/store_server.c
@@ -1,14 +1,31 @@
-#include "./../../include/crawler/store_server.h"
+#include "../../include/crawler/store_server.h"
 #include <stdio.h>
 #include <zlib.h>
 #include <stdlib.h>
 #include <string.h>
+#include <err.h>
 
 void save(char *filename, char *html)
 {
-    FILE *fp = fopen(filename, "w");
+    char repo[] = "./repository/";
+
+    char *dest = calloc(strlen(filename) + strlen(repo) + 1, sizeof(char));
+    if(dest == NULL)
+    {
+        errx(EXIT_FAILURE, "Out of memory\n");
+    }
+
+    strcpy(dest, repo);
+    strcat(dest, filename);
+
+    FILE *fp = fopen(dest, "w");
+    if(fp == NULL)
+    {
+        errx(EXIT_FAILURE, "failed to open the file\n");
+    }
 
     fputs(html, fp);
 
+    free(dest);
     fclose(fp);
 }
