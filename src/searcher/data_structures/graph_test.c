7a368530119ec27d6a337b75e59be389c06bd261
Lucien Lefauveau <lucien.lefauveau@epita.fr>
2023-02-09 18:49:15 +0100

    changed the testing file

--- src/searcher/data_structures/graph_test.c
+++ src/searcher/data_structures/graph_test.c
@@ -1,15 +1,32 @@
+#include <stdio.h>
 #include "graph.h"
 
 int main()
 {
-  int V = 5;
-  struct Graph* graph = createGraph(V);
-  addEdge(graph, 0, 1);
-  addEdge(graph, 0, 2);
-  addEdge(graph, 0, 3);
-  addEdge(graph, 1, 2);
+    printf("Let's create a web pages network graph !\n");
+    printf("How many web pages will be in it ?\n-> ");
+    int order;
+    scanf("%i", &order);
+    struct Graph* graph = createGraph(order);
+    printf("\nNow, let's link them ! You will have to: \n");
+    printf("- tell me the first node to link\n- press enter, \n- then the second one\n");
+    printf("If you added enough, just enter 0 and 0\n\n");
+    int node1;
+    int node2;
+    while(1)
+    {
+        printf("Enter the first node:\n");
+        scanf("%d", &node1);
+        printf("Enter the second node:\n");
+        scanf("%d", &node2);
+        if (node1 == 0 && node2 == 0)
+            break;
+        addEdge(graph, node1, node2);
+        printf("\nEnter a new couple :\n");
+    }
 
-  printGraph(graph);
+    printf("\nOk now lets see if it works :\n");
+    printGraph(graph);
 
-  return 0;
+    return 0;
 }
