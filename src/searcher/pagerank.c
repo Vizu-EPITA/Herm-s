4a154159277cc9ca3284661cda91e17c5fc675d2
Lucien Lefauveau <lucien.lefauveau@epita.fr>
2023-02-11 22:58:42 +0100

    Pagerank should be working

--- src/searcher/pagerank.c
+++ src/searcher/pagerank.c
@@ -7,25 +7,68 @@
 //HAS TO USE FLOATS
 void initRank(struct Graph* graph)
 {
-    for (int node = 0; node < graph->order; node++)
+    for(int node = 0; node < graph->order; node++)
     {
         graph->nodes[node]->pageRank = 1.0f/(double)graph->order;
     }
 }
 
+void rank(struct Graph* graph, int cycle)
+{
+    struct Node* curNode;
+    struct Node* prevNode;
+    double damp = 0.85f;
+    double sum;
+    for(;cycle > 0; cycle --)
+    {
+        for(int node = 0; node < graph->order; node++)
+        {
+            curNode = graph->nodes[node];
+            sum = 0;
+            for(int prev = 0; prev < curNode->nbPrev; prev++)
+            {
+                prevNode = curNode->prevList[prev];
+                sum += (double)prevNode->pageRank/(double)prevNode->nbAdj;
+            }
+            curNode->pageRank = (1.0f - damp) + damp * sum;
+        }
+    }
+}
+
+void printRank(struct Graph* graph)
+{
+    for(int node = 0; node < graph->order; node++)
+    {
+        printf("The page rank value of (%i) is [%lf] \n",
+                node, graph->nodes[node]->pageRank);
+    }
+}
+
 int main()
 {
-    struct Graph* graph = graphInit(4);
-    addEdge(graph, graph->nodes[0], graph->nodes[1]);
-    addEdge(graph, graph->nodes[0], graph->nodes[2]);
-    addEdge(graph, graph->nodes[1], graph->nodes[2]);
-    addEdge(graph, graph->nodes[2], graph->nodes[0]);
-    addEdge(graph, graph->nodes[1], graph->nodes[0]);
-    addEdge(graph, graph->nodes[3], graph->nodes[0]);
+    struct Graph* graph = graphInit(5);
+    addEdge(graph, graph->nodes[0], graph->nodes[3]);
+    addEdge(graph, graph->nodes[1], graph->nodes[3]);
+    addEdge(graph, graph->nodes[2], graph->nodes[3]);
+    addEdge(graph, graph->nodes[3], graph->nodes[4]);
 
+    printGraph(graph);
 
     initRank(graph);
-    printGraph(graph);
+    printf("\n\n");
+
+    rank(graph, 1);
+    printRank(graph);
+    printf("\n\n");
+
+    rank(graph, 5);
+    printRank(graph);
+    printf("\n\n");
+
+    rank(graph, 50);
+    printRank(graph);
+
+    freeGraph(graph);
 
     return 0;
 }
