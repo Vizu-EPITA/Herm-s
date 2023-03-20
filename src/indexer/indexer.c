476dd2d2f8f389b49f3c5f851f9f006b674d794b
lucien.lefauveau <lucien.lefauveau@epita.fr>
2023-03-20 15:42:41 +0100

    starting to implement parseWord()

--- src/indexer/indexer.c
+++ src/indexer/indexer.c
@@ -1,86 +1,32 @@
-#include <stdio.h>
+#include <err.h>
+#include <string.h>
 #include <stdlib.h>
-#include <libxml/HTMLparser.h>
 
-void extract_visible_text(xmlNode *node)
+void parseWord(char *page, char *wordBuf /*,[HTBLE]*/)
 {
-    if (node == NULL)
-    {
-        return;
-    }
-    if (node->type == XML_TEXT_NODE)
-    {
-        xmlChar *content = xmlNodeGetContent(node);
-        if (content != NULL)
-        {
-            printf("%s ", content);
-            xmlFree(content);
-        }
-    }
-    else if (node->type == XML_ELEMENT_NODE && xmlStrcmp(node->name, (const xmlChar *)"script") != 0)
-    {
-        extract_visible_text(node->children);
-    }
-    extract_visible_text(node->next);
 }
 
-void extract_hyperlinks(xmlNode *node)
+void parseText(char *page)
 {
-    if (node == NULL)
+    char *wordBuf = malloc(sizeof(char)*100);
+    while (*page != 0)
     {
-        return;
-    }
-    if (node->type == XML_ELEMENT_NODE && xmlStrcmp(node->name, (const xmlChar *)"a") == 0)
-    {
-        xmlChar *href = xmlGetProp(node, (const xmlChar *)"href");
-        if (href != NULL)
+        // End of file (EOF)
+        if (*page == 0)
+            return;
+        // Tag detected
+        else if (*page == '<')
         {
-            printf("Link: %s\n", href);
-            xmlFree(href);
-        }
+            // Search end of tag or EOF
+        while (*page != '>' && *page != 0)
+            page++;
     }
-    extract_hyperlinks(node->children);
-    extract_hyperlinks(node->next);
+    // End of tag
+    else if (*page == '>')
+        *page++;
+    else if (*page >= '0' && *page <= '9' ||
+             *page >= 'A' && *page <= 'Z' ||
+             *page >= 'a' && *page <= 'z')
+        parseWord(page, wordBuf);
 }
-
-int main(int argc, char **argv)
-{
-    if (argc != 2)
-    {
-        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
-        return EXIT_FAILURE;
-    }
-
-    xmlDocPtr doc;
-    htmlDocPtr html;
-    xmlNodePtr root;
-
-    // Parse the HTML file
-    html = htmlReadFile(argv[1], NULL, HTML_PARSE_NOBLANKS | HTML_PARSE_NOERROR | HTML_PARSE_NOWARNING);
-    if (html == NULL)
-    {
-        fprintf(stderr, "Error: could not parse file %s\n", argv[1]);
-        return EXIT_FAILURE;
-    }
-
-    // Convert HTML document to generic XML document
-    doc = (xmlDocPtr)html;
-    if (doc == NULL)
-    {
-        fprintf(stderr, "Error: could not convert HTML document to XML document\n");
-        return EXIT_FAILURE;
-    }
-
-    // Get the root element
-    root = xmlDocGetRootElement(doc);
-
-    // Extract visible text and hyperlinks
-    extract_visible_text(root);
-    printf("\n");
-    extract_hyperlinks(root);
-
-    // Free resources
-    xmlFreeDoc(doc);
-    xmlCleanupParser();
-    return EXIT_SUCCESS;
 }
