6eea58ac0b2b45e5e557770d2999fb0190b009a1
lucien.lefauveau <lucien.lefauveau@epita.fr>
2023-03-22 14:45:16 +0100

    ParseWord ok

--- src/indexer/indexer.c
+++ src/indexer/indexer.c
@@ -1,32 +1,70 @@
 #include <err.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-void parseWord(char *page, char *wordBuf /*,[HTBLE]*/)
+void printWord(char *wordBuf, size_t len)
 {
+    for (int i = 0; i < len; i++)
+    {
+        printf("%c", *wordBuf);
+        wordBuf++;
+    }
+    printf("\n");
+}
+
+size_t parseWord(char *page, char *wordBuf /*,[HTBLE]*/)
+{   
+    size_t len = 0;
+    while(*page != 0 &&
+          *page >= '0' && *page <= '9' ||
+          *page >= 'A' && *page <= 'Z' ||
+          *page >= 'a' && *page <= 'z')
+    {
+        *wordBuf = *page;
+        wordBuf++;
+        page++;
+        len++;
+    }
+    return len;
 }
 
 void parseText(char *page)
 {
     char *wordBuf = malloc(sizeof(char)*100);
+    size_t len;
     while (*page != 0)
     {
-        // End of file (EOF)
+    //printf("parseText loop\n");
+    // End of file (EOF)
         if (*page == 0)
             return;
         // Tag detected
         else if (*page == '<')
         {
             // Search end of tag or EOF
-        while (*page != '>' && *page != 0)
+            while (*page != '>' && *page != 0)
+            page++;
+        }
+        // End of tag
+        //else if (*page == '>')
+            //*page++;
+        else if (*page >= '0' && *page <= '9' ||
+                *page >= 'A' && *page <= 'Z' ||
+                *page >= 'a' && *page <= 'z')
+        {
+            len = parseWord(page, wordBuf);
+            printWord(wordBuf, len);
+            page += len;
+        }
+        else
             page++;
     }
-    // End of tag
-    else if (*page == '>')
-        *page++;
-    else if (*page >= '0' && *page <= '9' ||
-             *page >= 'A' && *page <= 'Z' ||
-             *page >= 'a' && *page <= 'z')
-        parseWord(page, wordBuf);
 }
+
+int main()
+{
+    char page[] = "<test> This has to be printed<not this>";
+    parseText(page);
+    return 0;
 }
