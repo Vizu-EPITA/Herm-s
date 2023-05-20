ead85ec99b684b1c99e3193ea38cbc46d3b144f7
Lucien Lefauveau <lucien.lefauveau@epita.fr>
2023-05-20 18:59:19 +0200

    Saving push

--- src/searcher/searcher.c
+++ src/searcher/searcher.c
@@ -73,39 +73,54 @@ LinkedList *get_word_list(char *query)
 	return list;
 }
 
-void search_query(char *query, HashTable *table_docId, HashTable *table_wordId, HashTable *table_inverted, Graph *graph)
+void search_query(char *query, HashTable *table_docId, HashTable *table_wordId, TYPE *INVERTED, Graph *graph)
 {
-	/*
 	LinkedList *wordlist = get_word_list(query);
-	LinkedList *iterator = wordlist->next;
-	size_t count = 0;
+	LNode *iterator = wordlist->head->next;
+	size_t nbWords = 0;
+
+	//Get all the word IDs and count the number of words
 	while (iterator != NULL)
 	{
-		count ++;
+		nbWords ++;
 		iterator->wordId = ht_search(table_wordId, iterator->word);
 		iterator = iterator->next;
 	}
-	//LINKLIST docIdArray[count];
+
+	//Query empty
+	if (nbWords == 0)
+	{
+		printf("Please search for at least one word\n");
+		return;
+	}
+
+	//Get all the docIds, not sorted yet
+	STRUCT_INVERTED_TYPE structInvertedArray[count];
 	for (int i = 0; i < count; i++)
 	{
-		//docIdArray[i] = ht_search(table_inverted, )
-	}*/
-}
-
-int main()
-{
-	/*
-	LinkedList *l = get_word_list(" first  second     third   ");
-	LNode *iterator = l->head->next;
-	while (iterator != NULL)
-	{
-		printf("%s\n", iterator->word);
-		iterator = iterator->next;
+		structInvertedArray[i] = INVERTED_SESARCH(INVERTED, getElement(wordlist, i)->word);
 	}
-	printf("%s\n", getElement(l,0)->word);
-	printf("%s\n", getElement(l,1)->word);
-	printf("%s\n", getElement(l,2)->word);
-	freeList(l);
-	return 0;
-*/
+
+	STRUCT_INVERTED_TYPE firstStruct = structInvertedArray[0];
+	size_t fullSize = firstStruct->size;
+	int32_t docIdFullArray = firstStruct->array;
+	size_t nbAdded;
+	int32_t docIdArray[fullSize];
+	int32_t docId;
+	//Iterate over the first table of docIds
+	for (int i = 0; i < fullSize; i++)
+	{
+		docId = docIdFullArray[i];
+		//Iterate over the structs
+		for (int j = 1; j < nbWords; j++)
+		{
+			//Iterate over the arrays of the structs
+			for (int k = 0; k < structInvertedArray[j]->size)
+			{
+
+			}
+		}
+	}
+
 }
+//inverted -> struct array
