2b686fe20541a8059246d7613e1a1323038cec6e
Lucien Lefauveau <lucien.lefauveau@epita.fr>
2023-05-22 01:11:17 +0200

    Lets push it this time

--- include/searcher/searcher.h
+++ include/searcher/searcher.h
@@ -4,6 +4,8 @@
 #include "../../tools/hash_table.h"
 #include "../../tools/graph.h"
 #include "../../tools/linked_list.h"
+#include "../../tools/forward_table.h"
+#include "../../tools/inverted_table.h"
 #include <err.h>
 #include <pthread.h>
 #include <stdio.h>
@@ -14,7 +16,7 @@
 
 LinkedList *get_word_list(char *query);
 
-double get_ten_rank(size_t *tenRankIndexArray, size_t index, int32_t *docIdArray, size_t nbAdded, double formerMax, Graph *graph);
+double get_ten_rank(int32_t *tenRankIndexArray, size_t index, int32_t *docIdArray, size_t nbAdded, double formerMax, Graph *graph);
 
-char **search_query(char *query, HashTable *table_docId, HashTable *table_wordId, HashTable *table_inverted,Graph *graph);
+char **search_query(char *query, ForwardTable *forward, HashTable *table_wordId, InvertedTable *inverted,Graph *graph);
 #endif
--- include/user_interface/user_interface.h
+++ include/user_interface/user_interface.h
@@ -2,21 +2,17 @@
 #define USER_INTERFACE
 
 #include "../searcher/searcher.h"
-//#include "../../tools/hash_table.h"
-//#include "./data_structures/graph.h"
-//#include "./data_structures/shared_queue.h"
-//#include "./data_structures/linked_list.h"
 #include <gtk/gtk.h>
 
 typedef struct {
-    HashTable *table_docId;
+    ForwardTable *forward;
     HashTable *table_wordId;
-    HashTable *table_inverted;
+    InvertedTable *table_inverted;
     Graph *graph;
     //GtkWidget *entry;
 } ButtonClickedData;
 
 void button_clicked(GtkWidget *widget, gpointer data);
 
-void GtkMain(HashTable *table_docId, HashTable *table_wordId, HashTable *table_inverted, Graph *graph);
+void GtkMain(ForwardTable *forward, HashTable *table_wordId, InvertedTable *table_inverted, Graph *graph);
 #endif
