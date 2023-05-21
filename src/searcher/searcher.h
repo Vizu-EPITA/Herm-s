791fc0b49f983ed92638027ae9aeb47c0ba208e1
Lucien Lefauveau <lucien.lefauveau@epita.fr>
2023-05-21 13:46:05 +0200

    ?

--- src/searcher/searcher.h
+++ src/searcher/searcher.h
@@ -15,5 +15,7 @@
 
 LinkedList *get_word_list(char *query);
 
+double get_ten_rank(size_t *tenRankIndexArray, size_t index, int32_t *docIdArray, size_t nbAdded, double formerMax, Graph *graph);
+
 char **search_query(char *query, HashTable *table_docId, HashTable *table_wordId, HashTable *table_inverted,Graph *graph);
 #endif
